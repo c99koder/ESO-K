d25d7b464e707e3ae2f7f5cafc1775289f38222d
Sam Steele <sam.steele@gmail.com>
2017-02-18 10:39:59 -0500

    Add gamepad UI support

--- KAddon/K.lua
+++ KAddon/K.lua
@@ -1,20 +1,25 @@
 KAddon = {}
 KAddon.name = "K"
 
+function KAddon.Install(interaction)
+  local PopulateChatterOption = interaction.PopulateChatterOption
+  function interaction.PopulateChatterOption(manager, controlID, optionIndex, optionText, optionType, ...)
+    if optionType == CHATTER_GOODBYE then
+      if optionText == GetString(SI_DEFAULT_QUEST_COMPLETE_DECLINE_TEXT) then
+        optionText = "JK!"
+      else
+        optionText = "K."
+      end
+    end
+
+     PopulateChatterOption(manager, controlID, optionIndex, optionText, optionType, ...)
+  end
+end
+
 function KAddon.OnAddOnLoaded(event, addonName)
   if addonName == KAddon.name then
-    local PopulateChatterOption = INTERACTION.PopulateChatterOption
-    function INTERACTION.PopulateChatterOption(manager, controlID, optionIndex, optionText, optionType, ...)
-      if optionType == CHATTER_GOODBYE then
-        if optionText == GetString(SI_DEFAULT_QUEST_COMPLETE_DECLINE_TEXT) then
-          optionText = "JK!"
-        else
-          optionText = "K."
-        end
-      end
-
-       PopulateChatterOption(manager, controlID, optionIndex, optionText, optionType, ...)
-    end
+    KAddon.Install(SYSTEMS:GetKeyboardObject(ZO_INTERACTION_SYSTEM_NAME))
+    KAddon.Install(SYSTEMS:GetGamepadObject(ZO_INTERACTION_SYSTEM_NAME))
   end
 end
  
